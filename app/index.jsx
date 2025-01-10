9fa64d4db15523f9e661525e5be73de467618b60
SiamIslamSagor <msiamislam12345@gmail.com>
2025-01-10 19:27:56 +0600

    setup native-wind and some assests

--- app/index.jsx
+++ app/index.jsx
@@ -4,21 +4,12 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className={"text-3xl font-pblack text-center"}>Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
+      <Link href="/profile" className="text-center text-blue-600">
         Go to Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
